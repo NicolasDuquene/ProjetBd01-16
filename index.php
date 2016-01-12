c4bdc398f590382a50b6accfd9de49eabe26fdea
root <nicolas.duquene@gmail.com>
2016-01-12 10:40:55 +0100

    Notification de commit de la branche master

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+	<title>Gestion des associations sportives - Accueil</title>
+	<meta charset="utf-8"/>
+	<style>
+	body {height,width:100%;background: #045FB4;}
+	</style>
+</head>
+
+</html>
